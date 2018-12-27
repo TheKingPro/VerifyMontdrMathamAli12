2a3e85b5bdc9c0c1ccc63cd51caa9ec167d719a6
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-27 20:32:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('message', async message => {
-          if(message.content.startsWith(prefix + "active")) {
+            if(message.content.startsWith("$تقديم")) {
               if(!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط ⛔ ** ");
             message.member.addRole(message.guild.roles.find("name". "Active"));
             message.author.send("** تم تفعيلك استمتع 🙂 **")
