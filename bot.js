633cdabecffb98dba606f5ee721b39a15b01626a
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-27 20:46:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "2";
+var prefix = "*";
+var adminprefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
