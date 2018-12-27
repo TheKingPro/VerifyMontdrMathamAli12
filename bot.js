3e87a0a8aac1cf9260def1c013e9522a291c9f25
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-27 20:24:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-const Discord = require("discord.js");
 
+const Discord = require("discord.js");
 const client = new Discord.Client();
+
 client.on('message', async message => {
           if(message.content.startsWith(prefix + "active")) {
               if(!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط ⛔ ** ");
