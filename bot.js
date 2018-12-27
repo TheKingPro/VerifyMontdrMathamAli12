5973915e40cb7666e23e60d7e9d5963609e063c4
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-27 20:48:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-          if(message.content.startsWith(prefix + "active")) {
-              if(!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط :no_entry: ** ");
+          if (message.content.startsWith(prefix + "active")) {
+              if (!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط :no_entry: ** ");
             message.member.addRole(message.guild.roles.find("name". "Active"));
             message.author.send("** تم تفعيلك استمتع :slight_smile: **")
           }
