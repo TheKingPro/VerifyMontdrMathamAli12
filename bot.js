33ab9b97f6d0507404eb433fca2b9a62cd005bf6
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-28 20:44:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('message', async message => {
             if(message.content.startsWith("*active")) {
+                                  message.delete()
               if(!message.channel.guild) return message.reply("** هذا الامر للسيرفرات فقط :no_entry: ** ");
             message.member.addRole(message.guild.roles.find("name", "Active"));
             message.author.send("** تم تفعيلك استمتع :slight_smile: **")
