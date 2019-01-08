4650f5067b2204a9613ef14819e3181da6c320b3
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2019-01-08 14:04:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('message', async message => {
             if(message.content.startsWith("*active")) {
                                   message.delete()
               if(!message.channel.guild) return message.reply("** هذا الامر للسيرفرات فقط :no_entry: ** ");
-            message.member.addRole(message.guild.roles.find("name", "Owner"));
+            message.member.addRole(message.guild.roles.find("name", "Active"));
             message.author.send("** تم تفعيلك استمتع :slight_smile: **")
           }
         });
